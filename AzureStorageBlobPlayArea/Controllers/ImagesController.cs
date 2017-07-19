1b428dd5b55a54185dfb85ba8df4f382ccbea93d
Sid <siddharthkpandey@gmail.com>
2017-07-19 23:23:16 +0100

    - initial commit

---
+++ AzureStorageBlobPlayArea/Controllers/ImagesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using AzureStorageBlobPlayArea.Services;
+
+namespace AzureStorageBlobPlayArea.Controllers
+{
+    public class ImagesController : Controller
+    {
+        private ImageStorageService _imageStoreService;
+
+        public ImagesController()
+        {
+            _imageStoreService = new ImageStorageService();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<ActionResult> Upload(HttpPostedFileBase image)
+        {
+            if (image != null)
+            {
+                string imageId = await _imageStoreService.SaveImage(image.InputStream);
+                return RedirectToAction("ShowImage", new {id = imageId});
+            }
+
+            return View("Index");
+        }
+
+        public ActionResult ShowImage(string id)
+        {
+            Uri imageUri = _imageStoreService.GetResourceUri(id);
+
+            return View(imageUri);
+        }
+    }
+}
---
+++ AzureStorageBlobPlayArea/Services/ImageStorageService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace AzureStorageBlobPlayArea.Services
+{
+    public class ImageStorageService
+    {
+        private readonly CloudBlobClient _cloudBlobClient;
+        private readonly Uri _blobServiceEndpoint = new Uri(ConfigurationManager.AppSettings.Get("blobServiceEndpoint"));
+
+        public ImageStorageService()
+        {
+            var credentials = new StorageCredentials("storageplayarea", "Ve4n5rfKu/ys899/86EdEUS3xrXT66ZRzm0mhzQy2thCv9HNQ98RqEn1VWoHsQ8o9iozfG20zg2NW/248xiVQA==");
+            _cloudBlobClient = new CloudBlobClient(_blobServiceEndpoint, credentials);    
+        }
+
+        public Uri GetResourceUri(string resourceId)
+        {
+            return new Uri(_blobServiceEndpoint, $"/images/{resourceId}");
+        }
+
+        public async Task<string> SaveImage(Stream imageInputStream)
+        {
+            string id = Guid.NewGuid().ToString();
+            CloudBlobContainer container = _cloudBlobClient.GetContainerReference("images");
+            CloudBlockBlob blob = container.GetBlockBlobReference(id);
+
+            await blob.UploadFromStreamAsync(imageInputStream);
+
+            return id;
+        }
+    }
+}
